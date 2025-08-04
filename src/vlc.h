989d20d5b03d053a0d76377e4516df9341230730
Peter Shkumatov <petunder@users.noreply.github.com>
2025-08-04 23:25:38 +0500

    Update vlc.h

--- src/vlc.h
+++ src/vlc.h
@@ -33,27 +33,25 @@ along with vlc-bittorrent.  If not, see <http://www.gnu.org/licenses/>.
 // Unified VLC Headers.
 // The order is important. vlc_common.h must be first.
 // All other .cpp files should include this file instead of individual vlc headers.
-extern "C" {
-    #include <vlc_common.h>
-    #include <vlc_plugin.h>
-    #include <vlc_objects.h>
-    #include <vlc_access.h>
-    #include <vlc_demux.h>
-    #include <vlc_dialog.h>
-    #include <vlc_es.h>
-    #include <vlc_fs.h>
-    #include <vlc_input.h>
-    #include <vlc_input_item.h>
-    #include <vlc_interface.h>
-    #include <vlc_interrupt.h>
-    #include <vlc_meta.h>
-    #include <vlc_playlist.h>
-    #include <vlc_stream.h>
-    #include <vlc_stream_extractor.h>
-    #include <vlc_threads.h>
-    #include <vlc_url.h>
-    #include <vlc_variables.h>
-}
+#include <vlc_common.h>
+#include <vlc_plugin.h>
+#include <vlc_objects.h>
+#include <vlc_access.h>
+#include <vlc_demux.h>
+#include <vlc_dialog.h>
+#include <vlc_es.h>
+#include <vlc_fs.h>
+#include <vlc_input.h>
+#include <vlc_input_item.h>
+#include <vlc_interface.h>
+#include <vlc_interrupt.h>
+#include <vlc_meta.h>
+#include <vlc_playlist.h>
+#include <vlc_stream.h>
+#include <vlc_stream_extractor.h>
+#include <vlc_threads.h>
+#include <vlc_url.h>
+#include <vlc_variables.h>
 
 #pragma GCC diagnostic pop
 
