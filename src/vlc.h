5c6b8626c68b6396c785b6747dc4ac04bce9205f
Peter Shkumatov <petunder@users.noreply.github.com>
2025-08-05 12:54:55 +0500

    Update vlc.h

--- src/vlc.h
+++ src/vlc.h
@@ -21,19 +21,20 @@
 #include <vlc_plugin.h>
 
 // 3. Остальные специфичные для плагина заголовки
+#include <vlc_objects.h> 
+#include <vlc_input.h>
+#include <vlc_stream_extractor.h>
 #include <vlc_access.h>
 #include <vlc_demux.h>
 #include <vlc_dialog.h>
 #include <vlc_es.h>
 #include <vlc_fs.h>
-#include <vlc_input.h>
 #include <vlc_input_item.h>
 #include <vlc_interface.h>
 #include <vlc_interrupt.h>
 #include <vlc_meta.h>
 #include <vlc_playlist.h>
 #include <vlc_stream.h>
-#include <vlc_stream_extractor.h>
 #include <vlc_threads.h>
 #include <vlc_url.h>
 #include <vlc_variables.h>
