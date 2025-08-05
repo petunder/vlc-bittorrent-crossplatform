c24d6eba1c5c5b755da2dd821780668790bf624a
Peter Shkumatov <petunder@users.noreply.github.com>
2025-08-05 12:48:11 +0500

    Update interface.cpp

--- src/interface.cpp
+++ src/interface.cpp
@@ -181,7 +181,7 @@ static void* Run(void* data) {
                 // Старый код: lt::from_hex(hash_str, (char*)active_hash.data()); (неверные аргументы)
                 // Новый код передает все 3 необходимых аргумента: hex-строку, ее длину (40)
                 // и указатель на выходной буфер.
-                lt::aux::from_hex(hash_str, 40, (char*)active_hash.data());
+                lt::aux::from_hex({hash_str, 40}, {reinterpret_cast<std::uint8_t*>(active_hash.data()), 20});
                 // --- КОНЕЦ ИЗМЕНЕНИЯ ---
                 free(hash_str);
             } else {
