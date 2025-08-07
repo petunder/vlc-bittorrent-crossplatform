ee664194a3c757317271cedb54ad1fb6dd2d096c
Peter Shkumatov <petunder@users.noreply.github.com>
2025-08-07 18:03:16 +0500

    Update overlay.h

--- src/overlay.h
+++ src/overlay.h
@@ -6,11 +6,9 @@
 #include "vlc.h"
 
 // Объявляем функции, которые будут вызываться VLC.
-// extern "C" гарантирует C-совместимость имен функций, что важно для плагинов.
 extern "C" {
     int Open(vlc_object_t*);
     void Close(vlc_object_t*);
-    picture_t* Filter(filter_t*, picture_t*);
 }
 
 #endif // VLC_BITTORRENT_OVERLAY_H
